a4671eb14d968703d08272fbbc1e90989a4ece40
Steven Brunwasser <sbrunwasser@gmail.com>
2013-10-01 03:52:45 -0400

    added a testing file

---
+++ test.c
@@ -0,0 +1,26 @@
+#include "chunktional.h"
+#include <stdio.h>
+
+datatype(MaybeInt, NoneInt, JustInt);
+datatypeempty(MaybeInt, NoneInt);
+datatypedefine(MaybeInt, JustInt, {int number;});
+
+void f(MaybeInt *m) {
+    caseof(m)
+        where(JustInt, j) printf("%d\n", j->number);
+        whereatom(NoneInt) printf("hello\n");
+    endcaseof;
+}
+
+int main() {
+    MaybeInt *x = mkdatatype(NoneInt);
+    MaybeInt *y = mkdatatype(JustInt, 10);
+
+    f(x);
+    f(y);
+
+    free(x);
+    free(y);
+
+    return 0;
+}
