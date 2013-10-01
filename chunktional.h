d813500cb57318a2ad2a4a4d8db34229e1399e92
Steven Brunwasser <sbrunwasser@gmail.com>
2013-10-01 03:05:09 -0400

    created a working comprehensive chunktional implementation

--- chunktional.h
+++ chunktional.h
@@ -1,18 +1,30 @@
 #ifndef CHUNKTIONAL_H
 #define CHUNKTIONAL_H
 
+#include <stdlib.h>
 #include <stdarg.h>
 
-#define datatype(dataname, ...) enum dataname { ## __VA_ARGS__ };
-#define datatypedefine(dataname, option) struct option { dataname type;
-#define datatypeend }
+#define datatype(dataname, ...) enum dataname { __VA_ARGS__ }; typedef struct { enum dataname type; } dataname;
+#define datatypedefine(dataname, option, information) struct option { enum dataname type; struct information; }; \
+	static inline dataname *mk ## option () { \
+		void *v = allocdatatype(option); \
+		((struct option *)v)->type = option; \
+		return v; \
+	} \
+	static inline dataname *init ## option (dataname *value, struct option raw) { \
+		*((struct option *)value) = raw; \
+		return value; \
+	}
+#define datatypeempty(dataname, option) datatypedefine(dataname, option, {})
 
-#define mkdatatype(option, ...) ((struct option) {option, ## __VA_ARGS__})
-#define allocdatatype(option) ((struct option *)calloc(1, sizeof(struct option)))
-#define assigndatatype(value, option, ...) value = mkdatatype(option, ## __VA_ARGS__)
+#define allocdatatype(option) ((void *)calloc(1, sizeof(struct option)))
+#define __mkdatatype(option, ...) ((struct option) {option, {__VA_ARGS__}})
+#define mkdatatype(option, ...) init ## option (mk ## option (), __mkdatatype(option, ## __VA_ARGS__))
 
-#define match(value) switch(value.type)
-#define case(value, option, result) case option: { struct option result = (struct option)value;
-#define caseend } break
+#define caseof(value) { void *___VALUE___ = value; switch(value->type) {
+#define endcaseof }}
+#define where(option, result) case option: { struct option *result = (struct option *)___VALUE___;
+#define whereis(option) case option: {
+#define endwhere ;} break
 
 #endif
